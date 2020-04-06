a0660d76592fd08a4f32ce4b8b3e4321dd51b115
Kenn Mutai <mutaikenn@gmail.com>
2020-04-06 18:01:04 +0300

    Connect routes, UI and static files

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// Register Page
+router.get("/register", (req, res) => res.render("register"));
+
+// Login Page
+router.get("/login", (req, res) => res.render("login"));
+
+module.exports = router;
