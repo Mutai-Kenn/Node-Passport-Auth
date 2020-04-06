2237f74b2baaeec5ab4f13cb091b7605318a2720
Kenn Mutai <mutaikenn@gmail.com>
2020-04-06 20:08:17 +0300

    Mongo Atlas connected

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const expressLayouts = require("express-ejs-layouts");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Serving static files(Css and images)
 app.use(express.static("public"));
+
+dotenv.config();
+// Connect to MongoDB Atlas
+mongoose.connect(
+  process.env.MONGO_DB,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to DB")
+);
 
 // EJS
 app.use(expressLayouts);
